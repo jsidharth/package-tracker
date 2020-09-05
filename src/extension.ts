80f4780a12d6f8eb8cb606bf5941b8a3c3166262
Sidharth J <sidharthjayaprakash93@gmail.com>
2020-09-04 23:59:33 -0700

    Fetch details of each dependency with the open source API

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from "fs";
+import * as Promise from "bluebird";
+import axios from "axios";
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,13 +12,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "package-tracker" is now active!');
-	console.log(vscode.workspace.rootPath);
 	fs.readFile(`${vscode.workspace.rootPath}/package.json`, (err, data) => {
 		
-		if(err) console.error('package.json parsing failed');
-
+		if (err) {
+			console.error('package.json parsing failed');
+		}
+		//TODO: ADD ERROR CHECKS
 		const parsedData = JSON.parse(data.toString());
-		console.log(parsedData["dependencies"]);
+		const dependencies = Object.keys(parsedData["dependencies"]);
+		const fetchedDependencyData: any = [];
+
+		Promise.mapSeries(dependencies, eachDependency => {
+			return axios.get(`https://api.npms.io/v2/package/${eachDependency}`).then(response => {
+				if(response.status === 200) {
+					fetchedDependencyData.push({
+						eachDependency: response.data
+					});
+				} else {
+					console.error(`API to fetch dependecy details failed with ${response.status}: ${response.statusText}`);
+				}
+			}).catch(err => {
+				//TODO: CHECK ERROR HANDLING
+				console.error('Data fetch failed!!');
+			});
+		}).then(() => {
+			console.log(fetchedDependencyData);
+			
+		});
 		
 	});
 	// The command has been defined in the package.json file
